3622d0cb308e3596db92bc8da28f80ade56bb45d
Sanjaybganesha <sbettad1@binghamton.edu>
2021-07-14 18:37:04 -0400

    add functions for controller

--- controller/myController.js
+++ controller/myController.js
@@ -5,58 +5,60 @@ var apiRoutes = require('../router/myrouter');
 const fs = require('fs');
 var balanceData=[];
 var transactionData = [];
-var unquiePayer = [];
+
+var spent={};
+var totalBalance=0;
+
+//[
+//  { "payer": "DANNON", "points": -200, "timestamp": "2020-10-31T15:00:00Z" },
+//  { "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" },
+//  { "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" },
+//  { "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" },
+//  { "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }
+//]
 // controller functions
 module.exports = {
-    //implementation of promise to reader large data
+    //using datafrom data.json remember to change it
     balanceData: function (req, res) {
-        
-        callMyPromise().then(function (result) {
-            console.log(result);
-            transactionData = result;
+        unquieTransactionData().then(function (result) {
+            res.send(JSON.stringify((result)));
+        });
+    },
+    addData: function (req, res) {
+        let data = req.body;
+        if (data.payer != null && data.payer != undefined && data.points != null && data.points != undefined && data.timestamp != null && data.timestamp != undefined) {
+            addtranasctionData(data);
+            addTotalBalance(data.points);
+            res.send("Message received");
+            console.log("transaction");
             console.log(transactionData);
-            unquiePayer=transactionData.map(item => item.payer).filter((value, index, self) => self.indexOf(value) === index);
-            console.log(unquiePayer);  
-            res.send(JSON.stringify(unquiePayer));
-    
-    });
-        //console.log(uniquePayer);
+        }
+        else {
+            res.send("Message failed");
+        }
     
     },
-    addData:function(req,res){
-    console.log("Got Body:",req.body);
-    data=req.body;
-    if(data.payer!=null && data.payer!=undefined){
-    if(data.points!=null && data.points!=undefined){
 
-    if(data.timestamp!=null && data.timestamp!=undefined){
-        transactionData.push(data);
-        console.log(transactionData);
-        res.send("Message received");
-    }
-    else{
-        res.send("Message failed");
 
-        }
-    }
-    else{
-    transactionData.push(req.body);
-    res.send("Message failed");
-    }
-    }
-    else{
-        res.send("Message failed");
+
+    spendData: function (req, res) {
+        spendValue = req.body.points;
+        if (spendValue != null && spendValue != undefined) {
+            let pointsToSpend = parseInt(req.body.points);// reading points taking it as int
+            console.log(pointsToSpend);
+            if (totalBalance < pointsToSpend) {
+                res.send("points to spend is greater than total points available");
+            }
+            else {
+                console.log("total Balance in spend Data " + totalBalance);
+                subTotalBalance(pointsToSpend);
+
+                res.send("points to spend");
+    
+            }
         }
     },
-    spendData:function (req, res) {
-        console.log("Got Body:",req.body);
-        data=req.body;
-        res.send("hi i am spend");
-    }
 };
-
-
-
 
 //Step 1: declare promise
 var myPromise = () => {
@@ -77,3 +79,37 @@ var callMyPromise = async () => {
     //anything here is executed after result is resolved
     return JSON.parse(result);
 };
+
+var callunquieTransactionData = async () => {
+    var result = await (unquieTransactionData());
+    //anything here is executed after result is resolved
+    return result;
+};
+
+//Step 1: declare promise
+var unquieTransactionData = () => {
+    return new Promise((resolve, reject) => {
+        let dataCount = {};
+        //saving in unique payers in dataCount obj
+        transactionData.forEach((element) => {
+            if (!dataCount[element.payer]) {
+                dataCount[element.payer] = 0;
+            }
+            dataCount[element.payer] += element.points;
+        });
+        resolve(dataCount); 
+    });
+};
+
+
+var addtranasctionData= function (data) {
+    transactionData.push(data);
+};
+
+var addTotalBalance= function (data) {
+    totalBalance += data.points;
+};
+var subTotalBalance= function (data) {
+    totalBalance -= data.points;
+};
+
--- router/myrouter.js
+++ router/myrouter.js
@@ -8,10 +8,6 @@ router.get('/balance', myappcontroller.balanceData);
 router.post('/add', myappcontroller.addData);
 router.post('/spend', myappcontroller.spendData);
 
-// router.get('/add', function (req, res) {
-//     res.send('hello world')
-
-// });
 
 // Export API routes
 module.exports = router;
