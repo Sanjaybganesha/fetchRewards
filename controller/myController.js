5ab05cc239b0737a3d3f0b9d264712b00a3ab3ae
Sanjaybganesha <sbettad1@binghamton.edu>
2021-07-14 23:30:12 -0400

    sorting and calculating spend points without negatives

--- controller/myController.js
+++ controller/myController.js
@@ -5,8 +5,6 @@ var apiRoutes = require('../router/myrouter');
 const fs = require('fs');
 var balanceData=[];
 var transactionData = [];
-
-var spent={};
 var totalBalance=0;
 
 //[
@@ -27,19 +25,19 @@ module.exports = {
     addData: function (req, res) {
         let data = req.body;
         if (data.payer != null && data.payer != undefined && data.points != null && data.points != undefined && data.timestamp != null && data.timestamp != undefined) {
+            let date = new Date(data.timestamp);
+            data.timestamp = date;
             addtranasctionData(data);
             addTotalBalance(data.points);
             res.send("Message received");
-            console.log("transaction");
-            console.log(transactionData);
+          //  console.log("transaction");
+           // console.log(transactionData);
         }
         else {
             res.send("Message failed");
         }
     
     },
-
-
 
     spendData: function (req, res) {
         spendValue = req.body.points;
@@ -51,33 +49,16 @@ module.exports = {
             }
             else {
                 console.log("total Balance in spend Data " + totalBalance);
-                subTotalBalance(pointsToSpend);
 
-                res.send("points to spend");
-    
+                sortTransactionData();
+               
+                console.log(transactionData);
+                let result=subTotal(pointsToSpend);
+                subTotalBalance(pointsToSpend);
+                res.send(result);
             }
         }
     },
-};
-
-//Step 1: declare promise
-var myPromise = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile('data/data.json', (err, data) => {
-            if(err){
-            reject("error");
-            }else{
-            resolve(data);
-            }
-        });
-    });
-};
-
-//Step 2: async promise handler
-var callMyPromise = async () => {
-    var result = await (myPromise());
-    //anything here is executed after result is resolved
-    return JSON.parse(result);
 };
 
 var callunquieTransactionData = async () => {
@@ -100,6 +81,66 @@ var unquieTransactionData = () => {
         resolve(dataCount); 
     });
 };
+
+var callsortTransactionData = async () => {
+    return await (sortTransactionData());
+};
+
+//Step 1: declare promise
+var sortTransactionData = () => {
+    return new Promise((resolve, reject) => {
+        transactionData.sort((a, b) => {
+            if (a.timestamp < b.timestamp) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+        resolve(); 
+    });
+};
+
+var subTotal = function (pointsToSpend){
+    //saving in unique payers in dataCount obj
+    let remaingPoints = 0;
+    let indexPoints = [];
+    let result = {};            
+        transactionData.forEach((element, index) => {
+            if (pointsToSpend > 0) {
+ 
+                if (element.points > pointsToSpend) {
+                   
+                    if (!result[element.payer]) {
+                        
+                        result[element.payer] = 0;
+                    }
+                    
+                   result[element.payer] = -(pointsToSpend);
+                   
+                    element.points = element.points - pointsToSpend;
+                    pointsToSpend -= element.points;
+                    
+                }
+            
+            
+                else {
+                
+                   
+                    if (!result[element.payer]) {
+                        result[element.payer] = 0;
+                    }
+
+                    result[element.payer] = -(element.points);
+            
+                    pointsToSpend = pointsToSpend - element.points;
+                    element.points = 0;
+                    indexPoints.push(index);
+                }
+            }
+        });
+        console.log(result);
+        return result;
+    }
 
 
 var addtranasctionData= function (data) {
